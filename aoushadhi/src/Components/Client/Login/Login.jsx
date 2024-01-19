319ff4f51163765ba5233d4bd2e88595e52aa039
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-01-19 13:48:51 +0530

    Login Page Modified

--- aoushadhi/src/Components/Client/Login/Login.jsx
+++ aoushadhi/src/Components/Client/Login/Login.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../../Assets/Logo.svg";
 import "./Login.css";
 
 const Login = () => {
+  const [login, setLogin] = useState("Sign In");
+
+  const inputFields = (
+    <>
+      <input type="text" placeholder="Username" />
+      <input type="number" placeholder="ID Number" />
+      <input type="password" placeholder="Password" />
+      {login === "Sign Up" && <input type="email" placeholder="Email" />}
+    </>
+  );
+
   return (
     <div className="Container-Login">
       <div className="Logo-Login">
         <div className="Image-Login">
           <img src={Logo} alt="Aoushadhi" />
         </div>
-        <div className="Heading-Login">
+        <div className="Heading-Login ">
           <h1>Aoushadhi</h1>
         </div>
       </div>
       <div className="Form-Login">
         <div className="Input-Login">
-          <input type="text" value="Username" />
-          <input type="text" value="Password" />
-          <input type="text" value="ID Number" />
+          {inputFields}
           <p>Forgot Password ?</p>
         </div>
+
         <div className="Button-Login">
-          <button type="submit">Sign In</button>
-          <button type="submit">Sign Up</button>
+          <button
+            type="submit"
+            className={`${login === "Sign In" ? "Dark" : "Light"}`}
+            onClick={() => setLogin("Sign In")}
+          >
+            Sign In
+          </button>
+          <button
+            type="submit"
+            className={`${login === "Sign In" ? "Light" : "Dark"}`}
+            onClick={() => setLogin("Sign Up")}
+          >
+            Sign Up
+          </button>
         </div>
-        <hr/>
+        <hr />
         <div className="Authentication-Login">
           <button type="submit">Sign In By Google</button>
         </div>
