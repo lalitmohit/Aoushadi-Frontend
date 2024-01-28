45769e1006eedde71815d6d1b18a2748f485b99a
krish2592 <sachink9225s@gmail.com>
2024-01-28 18:37:55 +0530

    Updated product page

--- src/Components/Client/Medicines/Medicine.jsx
+++ src/Components/Client/Medicines/Medicine.jsx
@@ -1,49 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Medicine.css";
 import { CartCard } from "../../../Import";
 import Product from "../../../Assets/Product.svg";
+import axios from 'axios';
 
 const Medicine = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProducts()
+  }, [] );
+
+  const getAllProducts = async (e) => {
+    const products = await axios.get('http://localhost:4000/product_data_get');
+    setProducts(products.data);
+  }
+  
   return (
     <div className="product-page">
-        <div className="heading-product">
-            <h1>Showing All Results</h1>
-        </div>
-        <div className="product-cards">
-        <CartCard
-        img={Product}
-        price="500"
-        name="Centaphil - The Best One"
-        desc="BY: AOUSHADHI PVT LIM."
-        quant="500 ml"
-        isCart={false}
-      />
-      <CartCard
-        img={Product}
-        price="500"
-        name="Centaphil - The Best One"
-        desc="BY: AOUSHADHI PVT LIM."
-        quant="500 ml"
-        isCart={false}
-      />
-      <CartCard
-        img={Product}
-        price="500"
-        name="Centaphil - The Best One"
-        desc="BY: AOUSHADHI PVT LIM."
-        quant="500 ml"
-        isCart={false}
-      />
-      <CartCard
-        img={Product}
-        price="500"
-        name="Centaphil - The Best One"
-        desc="BY: AOUSHADHI PVT LIM."
-        quant="500 ml"
-        isCart={false}
-      />
-        </div>
-      
+      <div className="heading-product">
+        <h1>Showing All Results</h1>
+      </div>
+      <div className="product-cards">
+        {products.map((item, index) => (
+          <CartCard key={index}
+            img={Product}
+            price={item.price}
+            name={item.item_name}
+            desc={item.discount}
+            quant={10}
+            isCart={false}
+          />
+        ))}
+      </div>
+
     </div>
   );
 };
