ece48093c2a896dfa8e138ba044a7ae8ab4fe592
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2024-02-05 14:26:54 +0530

    Corrected_Cart

--- src/Components/Client/Cart/CartPayment/CartPayment.jsx
+++ src/Components/Client/Cart/CartPayment/CartPayment.jsx
@@ -4,6 +4,10 @@ import { NavLink } from "react-router-dom";
 
 const CartPayment = ({ price, coupon, delivery }) => {
   console.log(price, coupon, delivery, "price distribution");
+  let charge = 100;
+  if (price == 0) {
+    charge = 0;
+  }
   return (
     <div className="Payment-Box">
       <div className="Total-Cart">
@@ -19,14 +23,16 @@ const CartPayment = ({ price, coupon, delivery }) => {
           <h2>Discount on MRP.</h2>
           <p>Rs. {coupon}</p>
         </div>
-        <div className="amount-box">
-          <h2>Delivery Charges</h2>
-          <p>Rs. {delivery}</p>
-        </div>
+        {price > 0 && (
+          <div className="amount-box">
+            <h2>Delivery Charges</h2>
+            <p>Rs. {delivery}</p>
+          </div>
+        )}
         <hr />
         <div className="amount-box-2">
           <h2>Amount to be Paid</h2>
-          <p>Rs. {price - coupon + delivery}</p>
+          <p>Rs. {price - coupon + charge}</p>
         </div>
         <hr />
       </div>
